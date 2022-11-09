f26cfeb37d8b3596c2f4304c54644234bc68140d
rahul-dudhane-shortlist <rahul.dudhane@shortlist.net>
2022-11-09 18:50:20 +0530

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-translate-app';
+  constructor(
+    public translate: TranslateService
+  ) {
+    translate.addLangs(['en', 'nl','hi','mr']);
+    translate.setDefaultLang('en');
+  }
+
+  switchLang(lang: string) {
+    this.translate.use(lang);
+  }
 }
